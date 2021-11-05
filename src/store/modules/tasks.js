868ce699f14fc6a84849463eda3363c0c28c4f96
Унжакова Екатерина <katerina.unz@gmail.com>
2021-11-05 19:25:32 +0700

    Созданы модули tasks и todos

---
+++ src/store/modules/tasks.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+import Vue from "vue";
+const api = process.env.BASE_URL;
+
+const state = () => ({
+    tasks: [],
+    taskError: "",
+});
+
+const getters = {
+    allCurrentTodoTasks: (state) =>
+        state.tasks.sort((a, b) => {
+            return a.created_at > b.created_at ? -1 : 1;
+        }),
+    allCurrentTasksLength: (state) => state.tasks.length,
+    doneTasks: (state, getters) =>
+        getters.allCurrentTodoTasks.filter((task) => task.is_completed),
+    doneTasksLength: (state, getters) => getters.doneTasks.length,
+    tasksError: (state) => state.taskError,
+    checkIfTasksCompleted: (state, getters) => {
+        if (getters.allCurrentTasksLength === getters.doneTasksLength) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+};
+
+const actions = {
+    async fetchTasks({ commit }, id) {
+        try {
+            commit("setTaskError", "");
+            // ?filter[0][]=executor_user_id&filter[0][]==&filter[0][]=1
+            const response = await axios.get(`${api}/Task/get-items`);
+
+            let currTasks = response.data.data.items.filter(
+                (task) => task.lists_id === id
+            );
+            commit("setTasks", currTasks);
+        } catch (e) {
+            commit("setTaskError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    async addTask({ commit }, newTask) {
+        try {
+            commit("setTaskError", "");
+            let attributes = {
+                ...newTask,
+                // executor_user_id: 1,
+            };
+            const response = await axios.post(`${api}/Task/create`, {
+                attributes: attributes,
+            });
+
+            commit("addNewTask", response.data.data.attributes);
+        } catch (e) {
+            commit("setTaskError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    async deleteTask({ commit }, taskId) {
+        try {
+            commit("setTaskError", "");
+            await axios.delete(`${api}/Task/delete/${taskId}`);
+            commit("deleteOneTask", taskId);
+        } catch (e) {
+            commit("setTaskError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    async toggleTaskCompletion({ commit }, task) {
+        try {
+            commit("setTaskError", "");
+            let attributes = {
+                ...task,
+                // executor_user_id: 1,
+            };
+            await axios.put(`${api}/Task/update/${task.id}`, {
+                attributes: attributes,
+            });
+        } catch (e) {
+            commit("setTaskError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+};
+
+const mutations = {
+    setTaskError: (state, error) => (state.taskError = error),
+    setTasks: (state, tasks) => (state.tasks = tasks),
+    addNewTask: (state, task) => state.tasks.unshift(task),
+    deleteOneTask: (state, taskId) =>
+        (state.tasks = state.tasks.filter((task) => task.id !== taskId)),
+};
+export default {
+    actions,
+    state,
+    getters,
+    mutations,
+};
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,6 +1,6 @@
-// import Vue from "vue";
 import axios from "axios";
-const url = process.env.BASE_URL;
+import Vue from "vue";
+const api = process.env.BASE_URL;
 
 const state = () => ({
     todos: [],
@@ -14,13 +14,24 @@ const getters = {
         state.todos.sort((a, b) => {
             return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
         }),
+    currentTodo: (state) => state.currentTodo,
+    allFilteredTodos: (state, getters) => {
+        switch (state.filterName) {
+            case "all":
+                return getters.allTodos;
+            case "completed":
+                return getters.allTodos.filter((todo) => todo.is_completed);
+            default:
+                return getters.allTodos.filter((todo) => !todo.is_completed);
+        }
+    },
     todosError: (state) => state.todoError,
 };
 
 const actions = {
     async fetchTodos({ commit }) {
         try {
-            const token = process.env.VUE_APP_SECRET_CODE;
+            const token = process.env.VUE_APP_TOKEN;
 
             if (token) {
                 axios.defaults.headers.common["Authorization"] =
@@ -28,18 +39,114 @@ const actions = {
             }
 
             commit("setError", "");
-            const response = await axios.get(`${url}/Lists/`);
+            const response = await axios.get(`${api}/Lists/get-items`);
 
             commit("setTodos", response.data.data.items);
         } catch (e) {
             commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    async fetchCurrentTodo({ commit }, id) {
+        try {
+            commit("setError", "");
+            const response = await axios.get(`${api}/Lists/${id}`);
+
+            commit("setCurrentTodo", response.data.data.attributes);
+        } catch (e) {
+            commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+            commit("setCurrentTodo", "");
+        }
+    },
+
+    async addTodo({ commit }, newTodoObj) {
+        try {
+            commit("setError", "");
+            const response = await axios.post(`${api}/Lists/create`, {
+                attributes: newTodoObj,
+            });
+            commit("addNewTodo", response.data.data.attributes);
+        } catch (e) {
+            commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    filterTodos({ commit }, optionName) {
+        commit("setFilteredTodos", optionName);
+    },
+
+    async deleteTodo({ commit }, todoId) {
+        try {
+            commit("setError", "");
+            await axios.delete(`${api}/Lists/delete/${todoId}`);
+            commit("deleteOneTodo", todoId);
+        } catch (e) {
+            commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    async todoListCompleted({ commit }, { todo, setTo }) {
+        try {
+            commit("setError", "");
+            await axios.put(`${api}/Lists/update/${todo.id}`, {
+                attributes: { ...todo, is_completed: setTo },
+            });
+
+            commit("setTodoListCompleted", { todoId: todo.id, setTo });
+        } catch (e) {
+            commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
+        }
+    },
+
+    // изменить список когда задача добавлена/удалена
+    async changeTodoData({ commit }, { todo, countTasks, completedSetTo }) {
+        try {
+            commit("setError", "");
+
+            await axios.put(`${api}/Lists/update/${todo.id}`, {
+                attributes: {
+                    ...todo,
+                    is_completed: completedSetTo,
+                    count_tasks: countTasks,
+                },
+            });
+
+            commit("changeAmountOfTasksinList", {
+                todoId: todo.id,
+                countTasks,
+            });
+            commit("setTodoListCompleted", {
+                todoId: todo.id,
+                setTo: completedSetTo,
+            });
+        } catch (e) {
+            commit("setError", e.message);
+            Vue.$vToastify.error(e.message, "Что-то пошло не так");
         }
     },
 };
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
+    setCurrentTodo: (state, todo) => (state.currentTodo = todo),
+    setFilteredTodos: (state, optionName) => (state.filterName = optionName),
     setError: (state, error) => (state.todoError = error),
+    addNewTodo: (state, todo) => state.todos.push(todo),
+    deleteOneTodo: (state, todoId) =>
+        (state.todos = state.todos.filter((todo) => todo.id !== todoId)),
+    setTodoListCompleted: (state, { todoId, setTo }) => {
+        state.todos.filter((todo) => todo.id === todoId)[0].is_completed =
+            setTo;
+    },
+    changeAmountOfTasksinList: (state, { todoId, countTasks }) => {
+        state.todos.filter((item) => item.id === todoId)[0].count_tasks =
+            countTasks;
+    },
 };
 
 export default {
